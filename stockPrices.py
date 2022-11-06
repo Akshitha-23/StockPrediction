cc088f00dd68c706f0d73690d98aefceda8dfa10
Akshitha Pathakoti <85157941+Akshitha-23@users.noreply.github.com>
2022-11-06 13:20:57 +0530

    Add files via upload

---
+++ stockPrices.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+st.title("Simple Stock Price App")
+tickerName = st.text_input("Enter name of the company ")
+tickerSymbol =st.text_input("Enter its ticker")
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='max')
+st.write( """Shown are the stock **closing price** and ***volume*** of""" ,tickerName )
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
